c090c115e246dad491fcd170bfab4b3185d3b21e
Nivalis-Git <s.n.shterenberg@gmail.com>
2021-11-08 16:21:57 -0500

    Commented js file for event handler

--- src/js/validate_password.js
+++ src/js/validate_password.js
@@ -1,33 +1,41 @@
+/* 
+ * Event handler for the password-submission button.
+ * Determines validity of the password and toggles appropriate response-text
+ */
 button_validatePass.onclick = function()
 {
-	var psw = document.getElementById("password").value;
-	var mask_psw = /.{3,5}-.{3,5}-.{3,5}/;
-	let valid = mask_psw.test(psw);
+	// I. Test password against input mask and set validity flag
+		var psw = document.getElementById("password").value;
+		var mask_psw = /.{3,5}-.{3,5}-.{3,5}/;
+		let valid = mask_psw.test(psw);
 	
-	var div_valid = document.getElementById("password_valid");
-	var div_invalid = document.getElementById("password_invalid");
-	
-	switch (valid)
-	{
-		case true:
-			if (div_valid.classList.contains("hidden"))
-			{
-				if (!div_invalid.classList.contains("hidden"))
+	// II. Toggle appropriate response-text
+		// i. Obtain the div elements
+		var div_valid = document.getElementById("password_valid");
+		var div_invalid = document.getElementById("password_invalid");
+		
+		// ii. Toggle control-area
+		switch (valid)
+		{
+			case true:    // Comments for this case apply to all other cases
+				if (div_valid.classList.contains("hidden"))    // Only toggle if the text is still hidden
+				{
+					if (!div_invalid.classList.contains("hidden"))    // Hide the other text if still present
+						div_invalid.classList.toggle("hidden");
+					div_valid.classList.toggle("hidden");    // Unhide response-text
+				}
+				break;
+			
+			case false:
+				if (div_invalid.classList.contains("hidden"))
+				{
+					if (!div_valid.classList.contains("hidden"))
+						div_valid.classList.toggle("hidden");
 					div_invalid.classList.toggle("hidden");
-				div_valid.classList.toggle("hidden");
-			}
-			break;
-		
-		case false:
-			if (div_invalid.classList.contains("hidden"))
-			{
-				if (!div_valid.classList.contains("hidden"))
-					div_valid.classList.toggle("hidden");
-				div_invalid.classList.toggle("hidden");
-			}
-			break;
-		
-		default:
-			break;
-	}
+				}
+				break;
+			
+			default:
+				break;
+		}
 }
